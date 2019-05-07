1af47095510712b0167e579ca5bcb85e55ffa791
fleonhardt <falk.leonhardt@gmx.de>
2019-05-07 13:47:47 +0200

    added x axis flipping

--- src/app/services/player.position.service.ts
+++ src/app/services/player.position.service.ts
@@ -7,7 +7,30 @@ export class PlayerPositionService {
     private canvasElm: ElementRef
   ) {}
 
+  public isRightPlayerRectangle(rectangle: Rectangle) {
+    return rectangle.left + rectangle.width < this.getLeftPlayerFieldOfPlayWidth();
+  }
 
+  public isLeftPlayerRectangle(rectangle: Rectangle) {
+    return this.getLeftPlayerFieldOfPlayWidth() < rectangle.left;
+  }
+
+  public isRightPlayerResponse(singleFaceResponse) {
+    return singleFaceResponse.faceRectangle.left + singleFaceResponse.faceRectangle.width < this.getLeftPlayerFieldOfPlayWidth();
+  }
+
+  public isLeftPlayerResponse(singleFaceResponse) {
+    return this.getLeftPlayerFieldOfPlayWidth() < singleFaceResponse.faceRectangle.left;
+  }
+
+  public getLeftPlayerFieldOfPlayWidth(): number {
+    return this.canvasElm.nativeElement.width / 2;
+  }
+
+
+
+
+  /*
   public isLeftPlayerRectangle(rectangle: Rectangle) {
     return rectangle.left + rectangle.width < this.getLeftPlayerFieldOfPlayWidth();
   }
@@ -27,4 +50,5 @@ export class PlayerPositionService {
   public getLeftPlayerFieldOfPlayWidth(): number {
     return this.canvasElm.nativeElement.width / 2;
   }
+  */
 }
